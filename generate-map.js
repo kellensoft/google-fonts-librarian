48eceeb400ee339a0233275b0ef11100faef1bbb
Kellen Sullivan <kellen@kellensoft.com>
2025-07-15 11:06:18 -0500

    Create generate-map.js

---
+++ generate-map.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const slugify = require('slugify');
+
+const API_KEY = process.env.GOOGLE_FONTS_API_KEY;
+const OUTPUT_FILE = path.join(__dirname, '../gFontLibrary.js');
+
+async function generateFontLibrary() {
+  try {
+    const { data } = await axios.get(
+      `https://www.googleapis.com/webfonts/v1/webfonts?key=${API_KEY}`
+    );
+
+    const fonts = {};
+
+    for (const font of data.items) {
+      const key = slugify(font.family, { lower: true });
+      fonts[key] = {
+        importUrl: `https://fonts.googleapis.com/css2?family=${font.family.replace(/ /g, '+')}&display=swap`,
+        cssFamily: `'${font.family}', ${font.category}`,
+      };
+    }
+
+    const exportString = `export const gFontLibrary = ${JSON.stringify(fonts, null, 2)};\n`;
+    fs.writeFileSync(OUTPUT_FILE, exportString);
+    console.log('✅ Font library updated.');
+  } catch (err) {
+    console.error('❌ Error generating font library:', err);
+    process.exit(1);
+  }
+}
+
+generateFontLibrary();
